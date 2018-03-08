715db142ffd65e69a5c5df16fc62fdc0d545d2c2
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2018-03-08 12:56:10 +0000

    Created function getSiblingsBySelector

--- dom-utility.js
+++ dom-utility.js
@@ -1,6 +1,5 @@
 (function (window) {
     'use strict';
-
     function getAncestorBySelector(elementNode, selectorCSS) {
 
     	//To each parent
@@ -11,10 +10,37 @@
     			return elementNode;
     		}
     	}
+
+    	return null;
+    }
+
+    function getSiblingsBySelector(elementNode, cssSelector) {
+    	//If there is no parent element, there is no sibling
+    	if(!elementNode.parentElement) {
+    		return null;
+    	}
+    	
+    	var siblings = elementNode.parentElement.children;
+    	var collection = []; //Okay, this is not a collection.
+
+    	for(var x=0; x<siblings.length; x++) {
+    		var sibling = siblings[x];
+    	
+    		if( 
+    			!elementNode.isSameNode(sibling)
+    			&& sibling.matches(cssSelector)
+			) 
+    		{
+    			collection.push(sibling);
+    		}
+    	}
+
+    	return collection;
     }
 
     window.$DU = {
-    	getAncestorBySelector: getAncestorBySelector
+    	getAncestorBySelector: getAncestorBySelector,
+    	getSiblingsBySelector: getSiblingsBySelector
     };
 
 }(window));
